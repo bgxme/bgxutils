e7f23c32135722062d54a8cecc91140a006d0a82
vishpradeep <vishpradeep@users.noreply.github.com>
2020-06-14 14:20:50 +0400

    Added printDebugInfo method

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const Auth = require('./src/Auth');
 const SecretManager = require('./src/SecretManager');
 const Database = require('./src/Database');
 
+const printDebugInfo = () => {
+    console.log('Process Environment', process.env);
+}
 module.exports = {
     Auth,
     Database,
-    SecretManager
+    SecretManager,
+    printDebugInfo
 };

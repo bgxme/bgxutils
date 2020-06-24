399d054aa78bec2c54cc4ede2b23e4a11f6697bf
vishpradeep <vishpradeep@users.noreply.github.com>
2020-06-24 11:09:28 +0400

    more lambda util fns

--- src/Lambda/index.js
+++ src/Lambda/index.js
@@ -11,6 +11,37 @@ const sendResponse = (status, body, cb) => {
     });
 };
 
+const getQueryVar = (varName, event) => {
+    let value = false;
+    if (
+        event.queryStringParameters
+        && event.queryStringParameters[varName]
+    ) {
+        value = event.queryStringParameters[varName];
+    }
+
+    return value;
+};
+
+const getPostVar = (varName, event) => {
+    if (event && event.body) {
+        let value = false;
+        const eventBody = JSON.parse(event.body);
+
+        if (eventBody !== null && eventBody !== undefined) {
+            if (eventBody[varName]) {
+                value = eventBody[varName];
+            }
+        }
+
+        return value;
+    }
+
+    return false;
+};
+
 module.exports = {
-    sendResponse
+    sendResponse,
+    getQueryVar,
+    getPostVar
 };
